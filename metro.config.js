1f30ed583c031457d38eea3d967fb5ed0e4582cd
Akhil Manoj <manoja2@vcu.edu>
2025-05-13 13:46:35 -0400

    chore: upgrade Expo SDK to 53
    - The LTS Expo GO app requires the Expo SDK to be
    version 53. Upgrading to this version and fixing
    other required dependency issues.
    - Adding custom config to the metro dependency as
    with the new upgrade to Expo 53, metro switched
    the package.json exports field support to be
    enabled by default which causes native node.js
    libraries(like stream, ws, etc.) that aren't
    available in React Native to cause errors.
    - Added npmrc file to the project to esure the
    legacy-peer-deps=true flag is set to true to avoid
    npm install errors with peer dependencies.

---
+++ metro.config.js
@@ -0,0 +1,14 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('@expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname, {
+  // [Web-only]: Enables CSS support in Metro.
+  isCSSEnabled: true,
+});
+
+// Add any custom config here
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs', 'cjs'];
+config.resolver.unstable_enablePackageExports = false;
+
+module.exports = config; 
